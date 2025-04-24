c160e37bf232589c39554fdbbdd509e4d81731d2
Pavel Smirnov <pavlik.smirnov.2003@mail.ru>
2025-04-24 19:01:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ecommerce/AppNavigation.kt
@@ -0,0 +1,57 @@
+package com.example.ecommerce
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ecommerce.pages.CategoryProductsPage
+import com.example.ecommerce.pages.ProductDetailsPage
+import com.example.ecommerce.screen.AuthScreen
+import com.example.ecommerce.screen.HomeScreen
+import com.example.ecommerce.screen.LoginScreen
+import com.example.ecommerce.screen.SignupScreen
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+@Composable
+fun AppNavigation(modifier: Modifier=Modifier){
+
+    val navController=rememberNavController()
+    GlobalNavigation.navController=navController
+    val isLogeedIn=Firebase.auth.currentUser!=null
+    val firstPage=if(isLogeedIn) "home" else "auth"
+
+   NavHost(navController=navController , startDestination = "auth") {
+       composable("auth"){
+           AuthScreen(modifier,navController)
+
+       }
+       composable("login"){
+           LoginScreen(modifier,navController)
+
+       }
+       composable("signup"){
+           SignupScreen(modifier,navController)
+
+       }
+       composable("home"){
+           HomeScreen(modifier,navController)
+       }
+       composable("category-products/{categoryId}"){
+           var categoryId=it.arguments?.getString("categoryId")
+           CategoryProductsPage(modifier,categoryId?:"")
+       }
+       composable("product-details/{productId}"){
+           var productId=it.arguments?.getString("productId")
+           ProductDetailsPage(modifier,productId?:"")
+       }
+   }
+
+
+}
+object GlobalNavigation{
+    lateinit var navController: NavHostController
+}
